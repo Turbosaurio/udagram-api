7c3f33ed975841ffb2e1fd5abbdd173fdb165de5
Reinhold <reinholandia@gmail.com>
2021-05-02 13:41:27 -0500

    jhjmn

--- src/server.ts
+++ src/server.ts
@@ -40,6 +40,7 @@ import {V0_FEED_MODELS, V0_USER_MODELS} from './controllers/v0/model.index';
   // Start the Server
   app.listen( port, () => {
     console.log( `server running service in ${port}` );
+    console.log( `server allows connections from ${config.url}` );
     console.log( `press CTRL+C to stop server` );
   } );
 })();
